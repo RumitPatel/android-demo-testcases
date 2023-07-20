e808daffabd01c2932d8fbf0e6b8c39e1d1fdc6a
RumitPatel <rumit.patel5@gmail.com>
2023-07-20 12:52:06 +0530

    some code clean in QuizNavigationTest.kt

--- app/src/androidTest/java/com/rumit/android_testcase_example/QuizNavigationTest.kt
+++ app/src/androidTest/java/com/rumit/android_testcase_example/QuizNavigationTest.kt
@@ -1,17 +1,10 @@
 package com.rumit.android_testcase_example
 
-import android.content.Intent
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.rumit.android_testcase_example.robots.main
 import com.rumit.android_testcase_example.robots.quizQuestions
 import com.rumit.android_testcase_example.robots.result
-import org.hamcrest.core.AllOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -38,12 +31,6 @@ class QuizNavigationTest {
             isOnResultScreen()
             nameIsShown(name)
             resultIsXOutOfTen(10)
-
-            Intents.init()
-            val expected = AllOf.allOf(IntentMatchers.hasAction(Intent.ACTION_SEND))
-            Espresso.onView(ViewMatchers.withId(R.id.btnShareScore)).perform(ViewActions.click())
-            Intents.intended(expected)
-            Intents.release()
         }
     }
 }
